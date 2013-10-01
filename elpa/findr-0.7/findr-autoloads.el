fe93c843148c58526c47faf9bd18db4d4522d05a
Patrick Rose <pjr0911025@googlemail.com>
2013-10-01 09:53:16 +0100

    Added company, ruby, and reset the load-path

---
+++ elpa/findr-0.7/findr-autoloads.el
@@ -0,0 +1,18 @@
+;;; findr-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("findr-pkg.el" "findr.el") (21066 35708
+;;;;;;  913648 733000))
+
+;;;***
+
+(provide 'findr-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; findr-autoloads.el ends here
--- init.el
+++ init.el
@@ -1,9 +1,8 @@
-(add-to-list 'load-path "~/.emacs.d/elpa")
-
 (add-hook 'after-init-hook 'ido-mode)
 (require 'package)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
 (package-initialize)
 
 (require 'haskell-mode-autoloads)
