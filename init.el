e4568c70778c3d2b976a4e1b441f0a84fa3f9f31
Patrick Rose <aca10pjr@shef.ac.uk>
2013-12-18 14:55:27 +0000

    Forced loading of package.el

--- init.el
+++ init.el
@@ -1,8 +1,10 @@
+(load-file "~/.emacs.d/package.el")
+
 (add-hook 'after-init-hook 'ido-mode)
-(require 'package)
+(require 'package)			
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+			 ("marmalade" . "http://marmalade-repo.org/packages/")
+			 ("melpa" . "http://melpa.milkbox.net/packages/")))
 (package-initialize)
 
 (require 'haskell-mode-autoloads)
